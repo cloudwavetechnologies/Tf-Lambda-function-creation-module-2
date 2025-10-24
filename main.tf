91446adfe22616e3649748149533de684b866a2f
cloudwavetechnologies <manjunath@cloudwavetechnologies.co.in>
2025-10-24 07:35:20 +0530

    Added source path

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 module "lambda_function" {
-  source              = "./modules/lambda_function"
-  function_name       = var.function_name
-  runtime             = var.runtime
-  handler             = var.handler
-  role_arn            = var.role_arn
-  source_path         = var.source_path
-  tags                = var.tags
+  source = "./lambda_function"
+
+  function_name = var.function_name
+  runtime       = var.runtime
+  handler       = var.handler
+  role_arn      = var.role_arn
+  source_path   = var.source_path
+  tags          = var.tags
 }
